df2945fd7983536a9b65ff75760cf6871a5b2259
samalash <samalash@udel.edu>
2024-04-09 22:54:16 -0400

    Feat: create detailed questions page

---
+++ src/pages/detailed-questions.tsx
@@ -0,0 +1,15 @@
+//import React, { useState } from 'react';
+import '../App.css';
+//import { Button, Form } from 'react-bootstrap';
+
+function DetailedQuestionsPage() {
+
+    return (
+        <div>
+
+        </div>
+    );
+
+}
+
+export default DetailedQuestionsPage;
