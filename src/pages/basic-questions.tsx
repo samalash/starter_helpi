8c401d4600d4355e4ae348adb17aed3209235158
samalash <samalash@udel.edu>
2024-04-09 22:33:16 -0400

    Feat: added basic questions page

---
+++ src/pages/basic-questions.tsx
@@ -0,0 +1,16 @@
+//import React, { useState } from 'react';
+import '../App.css';
+//import { Button, Form } from 'react-bootstrap';
+
+
+
+function BasicQuestionsPage() {
+    return (
+        <div>
+            
+        </div>
+    );
+
+}
+
+export default BasicQuestionsPage;
