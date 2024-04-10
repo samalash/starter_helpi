83b902a42176647a73cc84ab761c60c43ef963f1
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-04-09 23:24:36 -0400

    feat: removed CRA default page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -26,23 +26,7 @@ function Home() {
   }
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Sammy Alashoush</p>
-        <p>Drew Crispino</p>
-        <p>Maanav Rao</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
       <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
