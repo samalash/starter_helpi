8add775ff0f8de740237a54bff9fec08119735cf
Drew Crispino <andrewvcrispino@gmail.com>
2024-04-10 22:22:16 -0400

    Fixed Navbar links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,13 @@ function Header(){
 
 return(
   <Navbar bg="primary" variant="dark">
-    <Navbar.Brand href="#">Career Helpi</Navbar.Brand>
+    <Navbar.Brand href="#/">Career Helpi</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#">Home</Nav.Link>
+      <Nav.Link href="#/">Home</Nav.Link>
       <Nav.Link href="#/basic-questions">Basic Questions</Nav.Link>
       <Nav.Link href="#/detailed-questions">Detailed Questions</Nav.Link>
     </Nav>
-
       <Button className="ms-auto">Sign In</Button>
-
   </Navbar>
 );
 }
