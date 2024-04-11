6b73d7eaec5e9652d477efd751fa496c8f0576d4
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-04-10 23:30:04 -0400

    feat: updated questions

--- src/pages/basic-questions.tsx
+++ src/pages/basic-questions.tsx
@@ -16,13 +16,13 @@ function BasicQuestionsPage() {
             </div>
             <div>
                 <div className="flex-container mw-75 ml-90">
-                    <TrueFalseQuestionBlock question="I prefer a structured work environment with clear guidelines." selectedAnswers={selectedAnswers} index={0}    />
-                    <TrueFalseQuestionBlock question="I am more interested in practical, hands-on tasks than theoretical concepts." selectedAnswers={selectedAnswers} index={0}    />
-                    <TrueFalseQuestionBlock question="I feel comfortable speaking in front of large groups of people." selectedAnswers={selectedAnswers} index={0}    />
-                    <TrueFalseQuestionBlock question="I am passionate about making a positive impact on society through my work." selectedAnswers={selectedAnswers} index={0}    />
-                    <TrueFalseQuestionBlock question="I prefer a fast-paced work environment with frequent changes and challenges." selectedAnswers={selectedAnswers} index={0}    />
-                    <TrueFalseQuestionBlock question="I am naturally inclined towards leadership roles and enjoy taking charge of projects." selectedAnswers={selectedAnswers} index={0}    />
-                    <TrueFalseQuestionBlock question="I value job security and stability over higher earning potential." selectedAnswers={selectedAnswers} index={0}    />
+                    <TrueFalseQuestionBlock question="You prefer a structured and organized work environment with clear guidelines." selectedAnswers={selectedAnswers} index={0}    />
+                    <TrueFalseQuestionBlock question="You're more interested in practical, hands-on tasks than theoretical concepts." selectedAnswers={selectedAnswers} index={0}    />
+                    <TrueFalseQuestionBlock question="You feel comfortable speaking in front of large groups of people." selectedAnswers={selectedAnswers} index={0}    />
+                    <TrueFalseQuestionBlock question="You are passionate about making a positive impact on society through your work." selectedAnswers={selectedAnswers} index={0}    />
+                    <TrueFalseQuestionBlock question="You may not prioritize a fast-paced environment with frequent changes." selectedAnswers={selectedAnswers} index={0}    />
+                    <TrueFalseQuestionBlock question="You might not gravitate naturally towards leadership roles, but you might still be open to them." selectedAnswers={selectedAnswers} index={0}    />
+                    <TrueFalseQuestionBlock question="Job security and stability seem important to you." selectedAnswers={selectedAnswers} index={0}    />
                 </div>
                 <div>
 
