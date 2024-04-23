d8d710f7ddbc174618e5ec07cedde96a52d8b2aa
Drew Crispino <andrewvcrispino@gmail.com>
2024-04-23 14:54:12 -0400

    Made it so only option is selectable at a time for the basic questions

--- src/components/TrueFalseQuestionBlock.tsx
+++ src/components/TrueFalseQuestionBlock.tsx
@@ -8,16 +8,15 @@ function TrueFalseQuestionBlock({question, selectedAnswers, index}: {question: s
         <div className="ml-4 mw-20rem pt-10">
             <p>{question}</p>
             <div>
-                <input className="form-check-input" type="radio" />
+                <input className="form-check-input" type="radio" name={String(index)} />
                 <label className="form-check-label ml-2">
                     True
                 </label>
-                <input className="form-check-input ml-10" type="radio" />
+                <input className="form-check-input ml-10" type="radio" name={String(index)} />
                 <label className="form-check-label ml-2">
                     False
                 </label>
             </div>
-
         </div>
     )
 }
