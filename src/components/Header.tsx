b1deb516c342b1db91a5ce650fd715e6ef8308ba
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-04-23 15:17:49 -0400

    feat: added name to button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Header(){
+const name = localStorage.getItem("name");
 
 return(
   <Navbar bg="primary" variant="dark">
@@ -12,7 +13,7 @@ return(
       <Nav.Link href="#/basic-questions">Basic Questions</Nav.Link>
       <Nav.Link href="#/detailed-questions">Detailed Questions</Nav.Link>
     </Nav>
-      <Button className="ms-auto">Sign In</Button>
+      <Button disabled={name !== ""} className="ms-auto">{name === "" ? "Sign In" : name}</Button>
   </Navbar>
 );
 }
