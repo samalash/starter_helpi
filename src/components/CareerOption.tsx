f4be84e388e081d8095aea083235685f37bcc5e7
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-04-24 18:53:55 -0400

    Created career options component

---
+++ src/components/CareerOption.tsx
@@ -0,0 +1,8 @@
+export const CareerOptions = (props: { title: string, description: string }) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+};
