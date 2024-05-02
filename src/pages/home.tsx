8d471b7dc696c141f321eebcd5693c7cf16ae321
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-05-02 14:45:53 -0400

    feat: integrated tailwind css

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../App.css';
 import { Button } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
