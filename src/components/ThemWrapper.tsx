f22f2711ae927ccfdbab6d86dddb36213d18c776
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-05-06 12:36:27 -0400

    feat: removed attempted auto styling

--- src/components/ThemWrapper.tsx
+++ src/components/ThemWrapper.tsx
@@ -1,10 +1,8 @@
 const ThemeWrapper = ({ darkMode, children }: { darkMode:boolean, children: React.ReactNode }) => {
   return (
-    <body className={`${darkMode && "dark"}`}>
     <div className={darkMode ? "bg-slate-900 text-white" : "bg-white text-black"}>
       {children}
     </div>
-    </body>
   )
 }
 
