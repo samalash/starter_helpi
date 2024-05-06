474ccb85c2fd4b98561ad6a310c7b5c3baee8539
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-05-06 14:04:27 -0400

    feat: added fade in for the text as well in the career option component

--- src/components/CareerOption.tsx
+++ src/components/CareerOption.tsx
@@ -6,13 +6,15 @@ export const CareerOption = ({ title, description, darkMode }: {title: string, d
     <div>
       <FadeIn>
       <div className={`p-5 pb-3 shadow-xl rounded-xl ${darkMode ? "bg-[rgb(0,130,192)]" : "bg-[rgb(0,180,216)]"} text-white`}>
-        <h3 className="text-left">{title}</h3>
-        <p>{description}</p>
-        <h4>Here are some job listings that match this career:</h4>
-        <div>
-          <Button href={`https://www.indeed.com/jobs?q=${title}`} target="_blank" rel="noreferrer" variant={`${darkMode ? "dark" : "light"}`}>Indeed</Button>
-          <Button href={`https://www.linkedin.com/jobs/search/?keywords=${title}`} target="_blank" rel="noreferrer" variant={`${darkMode ? "dark" : "light"}`} className="m-4">LinkedIn Jobs</Button>
-        </div>
+        <FadeIn>
+          <h3 >{title}</h3>
+          <p>{description}</p>
+          <h4>Here are some job listings that match this career:</h4>
+          <div>
+            <Button href={`https://www.indeed.com/jobs?q=${title}`} target="_blank" rel="noreferrer" variant={`${darkMode ? "dark" : "light"}`}>Indeed</Button>
+            <Button href={`https://www.linkedin.com/jobs/search/?keywords=${title}`} target="_blank" rel="noreferrer" variant={`${darkMode ? "dark" : "light"}`} className="m-4">LinkedIn Jobs</Button>
+          </div>
+        </FadeIn>
       </div>
       <div className="pt-5"></div>
       </FadeIn>
