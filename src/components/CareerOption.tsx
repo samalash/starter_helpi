5679165299d6947ed8a1db640711986a20153508
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-05-05 21:01:07 -0400

    feat: changed links to buttons

--- src/components/CareerOption.tsx
+++ src/components/CareerOption.tsx
@@ -1,13 +1,18 @@
+import { Button } from "react-bootstrap";
+
 export const CareerOption = (props: { title: string, description: string }) => {
   return (
-        <a href={`https://www.indeed.com/jobs?q=${props.title}`} target="_blank" rel="noreferrer">Indeed</a>
-        <p></p>
-        <a href={`https://www.linkedin.com/jobs/search/?keywords=${props.title}`} target="_blank" rel="noreferrer">LinkedIn Jobs</a>
     <div>
       <div className="p-5 pb-3 shadow-xl rounded-xl bg-[rgb(0,180,216)] text-white">
         <h3 className="text-left">{props.title}</h3>
         <p>{props.description}</p>
         <h4>Here are some job listings that match this career:</h4>
+        <div>
+          <Button href={`https://www.indeed.com/jobs?q=${props.title}`} target="_blank" rel="noreferrer" variant="light">Indeed</Button>
+          <Button href={`https://www.linkedin.com/jobs/search/?keywords=${props.title}`} target="_blank" rel="noreferrer" variant="light" className="m-4">LinkedIn Jobs</Button>
+        </div>
+      </div>
+      <div className="pt-5"></div>
     </div>
   );
 };
