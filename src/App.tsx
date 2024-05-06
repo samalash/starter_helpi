8aa54910ee256ee9aca3ea726630167fd9d1291b
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-05-06 03:30:32 -0400

    feat: changed structure of wrapper so that everything had access to the theme state variable

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,27 @@ import BasicQuestionsPage from './pages/basic-questions';
 import DetailedQuestionsPage from './pages/detailed-questions';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ThemeWrapper from './components/ThemWrapper';
+import Header from './components/Header';
 
 
 function App() {
   const [reload, setReload] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<boolean>(localStorage?.theme === 'dark' ? true : false);
+
+  useEffect(() => {
+  localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+  }, [darkMode]);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ThemeWrapper><Home reload={reload} /></ThemeWrapper>} />
+        <Route path="/" element={
+          <ThemeWrapper darkMode={darkMode}>
+            <Header></Header>
+            <Home reload={reload} />
+          </ThemeWrapper>} />
         <Route path="/basic-questions" element={<BasicQuestionsPage setReload={setReload} />} />
         <Route path="/detailed-questions" element={<DetailedQuestionsPage setReload={setReload} />} />
       </Routes>
--- src/components/ThemWrapper.tsx
+++ src/components/ThemWrapper.tsx
@@ -1,11 +1,4 @@
-import { useEffect, useState } from "react";
-
-const ThemeWrapper = ({ children }: { children: React.ReactNode }) => {
-    const [darkMode, setDarkMode] = useState<boolean>(localStorage?.theme === 'dark' ? true : false);
-
-  useEffect(() => {
-  localStorage.setItem('theme', darkMode ? 'dark' : 'light');
-  }, [darkMode]);
+const ThemeWrapper = ({ darkMode, children }: { darkMode:boolean, children: React.ReactNode }) => {
   return (
     <span className={`${darkMode && "dark"} dark:bg-slate-900`}>{children}</span>
   )
