4c285b64831b589349dd0573c38ef6f11c0a2527
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-05-05 21:00:48 -0400

    feat: changed results to card layout

--- src/components/CareerOption.tsx
+++ src/components/CareerOption.tsx
@@ -1,12 +1,13 @@
 export const CareerOption = (props: { title: string, description: string }) => {
   return (
-    <div className="pt-10">
-      <h3 className="text-left">{props.title}</h3>
-      <p>{props.description}</p>
-      <h4>Here are some job listings that match this career:</h4>
         <a href={`https://www.indeed.com/jobs?q=${props.title}`} target="_blank" rel="noreferrer">Indeed</a>
         <p></p>
         <a href={`https://www.linkedin.com/jobs/search/?keywords=${props.title}`} target="_blank" rel="noreferrer">LinkedIn Jobs</a>
+    <div>
+      <div className="p-5 pb-3 shadow-xl rounded-xl bg-[rgb(0,180,216)] text-white">
+        <h3 className="text-left">{props.title}</h3>
+        <p>{props.description}</p>
+        <h4>Here are some job listings that match this career:</h4>
     </div>
   );
 };
