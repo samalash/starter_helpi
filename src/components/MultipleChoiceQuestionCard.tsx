f854622e1db50f587d915dd216b98e77539ea385
samalash <samalash@udel.edu>
2024-05-07 20:20:34 -0400

    Feat: copy over props and states from TrueFalseQuestionCard

--- src/components/MultipleChoiceQuestionCard.tsx
+++ src/components/MultipleChoiceQuestionCard.tsx
@@ -2,3 +2,19 @@ import FadeIn from './FadeIn';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 
+
+function MultipleChoiceQuestionCard({questions, possibleAnswers, handleAnswerChange, selectedAnswers, darkMode}: {questions: string[], possibleAnswers: string[], handleAnswerChange:(index:number,answer:string) => void, selectedAnswers: string[], darkMode: boolean}) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNext = () => {
+        if (currentIndex < questions.length-1){
+            setCurrentIndex(currentIndex+1);
+        }
+    }
+
+    const handlePrevious = () => {
+        if (currentIndex > 0){
+            setCurrentIndex(currentIndex-1);
+        }
+    }
+export default MultipleChoiceQuestionCard;
