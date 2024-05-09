c50f9712b470736053def2f7091b1ba12c803390
samalash <samalash@udel.edu>
2024-05-08 21:05:58 -0400

    Style: remove useless divs and fix indentation

--- src/pages/detailed-questions.tsx
+++ src/pages/detailed-questions.tsx
@@ -151,34 +151,27 @@ function DetailedQuestionsPage({setReload, darkMode}: {setReload: (value: boolea
     
     return (
         <>
-            <div className="pb-3">
-            </div>
-            <div>
-                <div className="flex-container mw-75 mx-auto">
-                    <MultipleChoiceQuestionCard questions={questions} possibleAnswers={possibleAnswers} handleAnswerChange={handleAnswerChange} selectedAnswers={selectedAnswers} darkMode={darkMode} />
+            <div className="flex-container mw-75 mx-auto mb-24">
+                <MultipleChoiceQuestionCard questions={questions} possibleAnswers={possibleAnswers} handleAnswerChange={handleAnswerChange} selectedAnswers={selectedAnswers} darkMode={darkMode} />
+                <p className="text-center">
+                    <Button className="mt-5" onClick={handleQuizSubmit} disabled={processing || selectedAnswers.includes("")}>
+                        {processing ? 
+                        <Spinner></Spinner> :
+                        "Submit Answers"}
+                    </Button>
+                </p>
+                {showKeyErrorMessage &&
+                <div className="flex-container">
+                    <p className="mx-auto my-auto">
+                        <b>Error:</b> Please enter a valid OpenAI API key in the footer below and resubmit the quiz.
+                    </p>
                 </div>
-                <div className="mb-5">
-                    <p className="text-center">
-                        <Button className="mt-5" onClick={handleQuizSubmit} disabled={processing || selectedAnswers.includes("")}>
-                            {processing ? 
-                            <Spinner></Spinner> :
-                            "Submit Answers"}
-                        </Button>
-                    </p>
-                    {showKeyErrorMessage &&
-                    <div className="flex-container">
-                        <p className="mx-auto my-auto">
-                            <b>Error:</b> Please enter a valid OpenAI API key in the footer below and resubmit the quiz.
-                        </p>
-                    </div>
-                    }
-                    {resultCreated &&
-                    <p className="mw-75 mx-auto border border-primary border-3 rounded p-3">
-                        {detailedQuestionsResultsArrayFormatted.map((option, index) => (
-                            <CareerOptionQuizPages key={index} title={option.title} description={option.description} darkMode={darkMode} />
-                        ))
-                        }
-                    </p>
+                }
+                {resultCreated &&
+                <div className="mb-6">
+                    {detailedQuestionsResultsArrayFormatted.map((option, index) => (
+                        <CareerOptionQuizPages key={index} title={option.title} description={option.description} darkMode={darkMode} />
+                    ))
                     }
                 </div>
                 }
